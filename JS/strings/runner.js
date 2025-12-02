4668f809fd31095e2782f03422cdc91cbae40750
aish0629 <aishified@outlook.com>
2025-12-02 22:02:22 +0530

    Added negative test cases

--- JS/strings/runner.js
+++ JS/strings/runner.js
@@ -4,6 +4,7 @@
 import * as sm from './string_manipulation.js';
 
 function runStringManipulationTests() {
+    console.log('\n--- positive case tests ---');
   console.log('reverseString("hello") ->', sm.reverseString('hello'));
   console.log('isPalindrome("A man a plan a canal Panama") ->', sm.isPalindrome('A man a plan a canal Panama'));
   console.log('charFrequency("abbc") ->', Object.fromEntries(sm.charFrequency('abbc')));
@@ -14,6 +15,31 @@ function runStringManipulationTests() {
   console.log('firstNonRepeatingChar("swiss") ->', sm.firstNonRepeatingChar('swiss'));
   console.log('sumArray([1,"2",3]) ->', sm.sumArray([1,'2',3]));
   console.log('hasDuplicates([1,2,3,1]) ->', sm.hasDuplicates([1,2,3,1]));
+
+  // negative / edge-case tests
+  console.log('\n--- negative / edge-case tests ---');
+  function safeCall(fn, label, ...args) {
+    try {
+      const res = fn(...args);
+      console.log(`${label} ->`, res);
+    } catch (e) {
+      console.log(`${label} -> Error:`, e && e.message ? e.message : e);
+    }
+  }
+
+  safeCall(sm.reverseString, 'reverseString(null)', null);
+  safeCall(sm.reverseString, 'reverseString(123)', 123);
+  safeCall(sm.isPalindrome, 'isPalindrome(null)', null);
+  safeCall(sm.charFrequency, 'charFrequency(null)', null);
+  safeCall(sm.removeDuplicatesFromArray, 'removeDuplicatesFromArray(null)', null);
+  safeCall(sm.removeDuplicatesFromArray, 'removeDuplicatesFromArray("not an array")', 'not an array');
+  safeCall(sm.secondLargest, 'secondLargest([])', []);
+  safeCall(sm.secondLargest, 'secondLargest([1])', [1]);
+  safeCall(sm.isNumeric, 'isNumeric("12a3")', '12a3');
+  safeCall(sm.reverseWords, 'reverseWords("")', '');
+  safeCall(sm.firstNonRepeatingChar, 'firstNonRepeatingChar("")', '');
+  safeCall(sm.sumArray, 'sumArray([1,"x",3])', [1,'x',3]);
+  safeCall(sm.hasDuplicates, 'hasDuplicates(null)', null);
 }
 
 // run when executed directly
